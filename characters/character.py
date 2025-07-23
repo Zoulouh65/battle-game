0733c87268a13134414e2850e62fbfe0621081ba
Thomas Alcaraz <thomas.alcaraz31@gmail.com>
2025-07-23 21:59:35 +0200

    premier push

---
+++ characters/character.py
@@ -0,0 +1,17 @@
+# classe de base pour tous les personnages
+class Character:
+    def __init__(self, name, hp, weapon, armor):
+        self.name = name
+        self.hp = hp
+        self.weapon = weapon
+        self.armor = armor
+
+# attaquer un autre personnage
+    def attack(self, target):
+        damage = self.weapon.damage - target.armor.defense
+        if damage < 0:
+            damage = 0
+        
+        target.hp = target.hp - damage
+
+        print(self.name, "attaque", target.name, "avec", self.weapon.name)
