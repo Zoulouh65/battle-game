a936fcb34766ecca97206cd31023a5476a00e563
Throat Nightmare <selves.pierre@gmail.com>
2025-07-25 14:46:03 +0200

    ajout attaque

--- characters/Barbarian.py
+++ characters/Barbarian.py
@@ -4,3 +4,12 @@ from characters.character import Character
 class Barbarian(Character):
     def __init__(self, name, hp, Weapon, Armor):
         Character.__init__(name, hp, Weapon, Armor)
+
+    def attack(self, target):
+        damage = 2*(self.Weapon.damage) - target.Armor.defense
+        if damage < 0:
+            damage = 0
+
+        target.hp = target.hp - damage
+        
+        print(self.name, "attack", target.name, "with", self.Weapon.name)
