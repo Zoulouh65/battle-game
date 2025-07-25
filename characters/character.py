a7a72dc99bf3c0944f7239e6c7ec5de3b24848f0
Zoulouh65 <bstrazzera@an21.fr>
2025-07-25 14:14:06 +0200

    push 1.3

--- characters/character.py
+++ characters/character.py
@@ -16,9 +16,11 @@ class Character:
         
         print(self.name, "attack", target.name, "with", self.Weapon.name)
 
+
 class Barbarian(Character):
     def __init__(self, name, hp, Weapon, Armor):
         Character.__init__(name, hp, Weapon, Armor)
+
 
 
 class Wizard(Character):
