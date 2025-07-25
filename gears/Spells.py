8b454f1b7945a95bb7134ea2fe8ee49e62e91795
Zoulouh65 <bstrazzera@an21.fr>
2025-07-25 14:27:30 +0200

    push 1.0

---
+++ gears/Spells.py
@@ -0,0 +1,7 @@
+# Définittion des spells
+
+class Spells:
+    def __init__(self, name, damage, mana_cost):
+        self.name = name            # nom du spell
+        self.damage = damage        # dégâts du spell
+        self.mana_cost = mana_cost  # Coût en mana du spell
