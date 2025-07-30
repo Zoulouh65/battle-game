cff840f2175dab859f9fc831a21862f674232814
Zoulouh65 <bstrazzera@an21.fr>
2025-07-30 08:49:40 +0200

    PuSH 3.1

--- characters/Barbarian.py
+++ characters/Barbarian.py
@@ -5,14 +5,9 @@ class Barbarian(Character):
     def __init__(self, name:str, hp:int, Weapon:str, Armor:str):
         Character.__init__(self,name, hp, Weapon, Armor)
 
-    def attack(self, target):
-        for i in range(0,1) :
-            damage = (self.Weapon.damage) - target.Armor.defense
-            if damage < 0:
-                damage = 0
-            i += 1
-
+    # attaquer un autre personnage
+    def attack(self, target:str):
+        damage = (self.Weapon.damage *(100 - target.Armor.defense))/100
         target.hp = target.hp - damage
-
         print(self.name, "attack", target.name, "with", self.Weapon.name)
         print(f"{target.name} takes {damage} damage. ({target.hp} HP left)")
--- characters/character.py
+++ characters/character.py
@@ -9,9 +9,7 @@ class Character:
 # attaquer un autre personnage
     def attack(self, target:str):
         damage = (self.Weapon.damage *(100 - target.Armor.defense))/100
-
         target.hp = target.hp - damage
-        
         print(self.name, "attack", target.name, "with", self.Weapon.name)
         print(f"{target.name} takes {damage} damage. ({target.hp} HP left)")
 
